3332336b7640ff34f3a2194aef5b21f5ac6c63e2
Brycen Vasquez <brycenvasquez22@gmail.com>
2023-05-20 10:54:56 -0600

    Arrays complete

--- arrays.js
+++ arrays.js
@@ -3,12 +3,12 @@
 
 const faveColors = ['red', 'green', 'black']
 
-//CODE HERE
+const colorCopy = faveColors.slice(0,3)
 
 //////////////////PROBLEM 2////////////////////
 /* Add a fourth color to the end of the 'colorCopy' array using the push method. */
 
-//CODE HERE
+colorCopy.push('blue')
 
 //////////////////PROBLEM 3////////////////////
 /*
@@ -18,8 +18,7 @@ const faveColors = ['red', 'green', 'black']
 
 const numbers = [1, 2, 3, 4, 5]
 
-//CODE HERE
-
+const middleNums = numbers.slice(1,4)
 
 //////////////////PROBLEM 4////////////////////
 /* 
@@ -32,5 +31,12 @@ const numbers = [1, 2, 3, 4, 5]
 */
 
 const bigOrSmallArray = [1, 99, 42, 69, 102, -10, 159, 352]
+const answers = []
 
-// CODE HERE
+for (let i = 0; i < bigOrSmallArray.length; i++) {
+  if (bigOrSmallArray[i] > 100) {
+    answers.push('big')
+  } else {
+      answers.push('small')
+  }
+}
